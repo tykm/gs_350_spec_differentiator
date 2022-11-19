37a631a36fe15cfb7845ce651c4108404b0485f4
tykm <tylerkim01@gmail.com>
2022-11-18 19:11:32 -0500

    Testing output

--- spec_differentiator.py
+++ spec_differentiator.py
@@ -14,3 +14,12 @@ trim_specs = [
     "Rear Air Conditioning Controls; Rear Audio Controls; [CK] Cold Weather Pkg; Front Seats Memory; Adaptive Front Lighting System; Night Vision Monitor; [PM] Premium Pkg; Dual 18-Way Power w/Dual 4-Way Power Lumbar Seat Adjusters; Semi-Aniline Perforated Leather Seat Trim; Leather-Wrapped & Wood Steering Wheel; Manual Rear Side Sun Shades; Adaptive Variable Suspension; P235/45R18 Tires; Split 9-Spoke Alloy Wheels"
 ]
 
+# Dummy check
+if len(trim_names) != len(trim_specs):
+    print("Amount of trim names and specs do not match. Please recheck configuration")
+
+# Loop through trims
+for i in range(len(trim_names)):
+    print(trim_names[i])
+    print(format(trim_specs[i]))
+    print()
